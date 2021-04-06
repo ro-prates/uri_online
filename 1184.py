bf0896f1821d07c217656f43ea085485b17b975c
ro-prates <ronaldy1995@hotmail.com>
2021-04-05 22:31:49 -0300

    100%

---
+++ 1184.py
@@ -0,0 +1,20 @@
+matriz = []
+
+operacao = input()
+
+for l in range(12):
+    linha = []
+    for c in range(12):
+        linha.append(float(input()))
+    matriz.append(linha)
+
+resultado = 0.0
+for l in range(12):
+    for c in range(12):
+        if l > c:
+            resultado += matriz[l][c]   
+if operacao == "S" or operacao == "s":
+    print(f'{resultado:.1f}')
+elif operacao == "M" or operacao == "m":
+    resultado = resultado / 66
+    print(f'{resultado:.1f}')
